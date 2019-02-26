39bb1977f50cbc21ee8a9a3fc76dd2603e033db0
John Young <john.young@spreadstreet.io>
2019-02-26 12:29:34 -0500

    Finished all drills, moving to recursive functions

--- jy/is-it-prime.js
+++ jy/is-it-prime.js
@@ -1,6 +1,7 @@
 'use strict';
 
 function isPrime(n) {
+  let ticks = 0;
   // if n is less than 2 or a decimal, it's not prime
   if (n < 2 || n % 1 != 0) {
     return false;
@@ -8,10 +9,17 @@ function isPrime(n) {
   // otherwise, check if `n` is divisible by any integer
   // between 2 and n.
   for (let i = 2; i < n; ++i) {
+    ticks++;
     if (n % i == 0) return false;
   }
+  console.log(ticks);
   return true;
 }
 
-// Big O notation: 
-// Why: 
+console.log(isPrime(5));
+console.log(isPrime(7));
+console.log(isPrime(11));
+
+// Big O notation: O(n);
+// Why: Operations increase by exactly the amount of the number 
+// vs. the previous prime number if it is a prime;
